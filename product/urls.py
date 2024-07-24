a4021023cf476601d59911bc60379d1a9b5e3b5b
deepak31 <deepak.it22@bitsathy.ac.in>
2024-07-24 09:53:12 +0530

    order page

--- product/urls.py
+++ product/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from  . import views
 
 urlpatterns = [
-    path('',views.order,name='order'),
-    path('home/',views.home,name='home'),
     path('login/',views.login,name='login'),
+    path('order/',views.order,name='order'),
+    path(' ',views.home,name='home'),
     path('signup/',views.signup,name='login'),
     path('logout/',views.logout,name='logout'),
 
