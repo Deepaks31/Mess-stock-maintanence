50ec92f1dcb4127e0de705b644ef81a48aa7f0fa
deepak31 <deepak.it22@bitsathy.ac.in>
2024-07-25 14:34:30 +0530

    updated

--- product/urls.py
+++ product/urls.py
@@ -4,7 +4,7 @@ from  . import views
 urlpatterns = [
     path('login/',views.login,name='login'),
     path('order/',views.order,name='order'),
-    path(' ',views.home,name='home'),
+    path('',views.home,name='home'),
     path('signup/',views.signup,name='login'),
     path('logout/',views.logout,name='logout'),
 
